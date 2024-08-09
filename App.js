f12e817773ff0df17b3fe4173be39b911c253443
A-money <85850317+amariparris@users.noreply.github.com>
2024-08-09 16:06:28 -0400

    a6 commit

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import UserRoutes from "./User/routes.js";
 import cors from "cors";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
